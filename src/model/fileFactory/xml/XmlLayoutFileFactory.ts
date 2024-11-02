4d8ebf0dc464e12bae0efca56f0e44349e56df05
Cyril Leblanc <83089143+CyrilLeblanc@users.noreply.github.com>
2024-11-02 16:28:53 +0100

    Ask the user which layout type to chose to create when creating a layout file

--- src/model/fileFactory/xml/XmlLayoutFileFactory.ts
+++ src/model/fileFactory/xml/XmlLayoutFileFactory.ts
@@ -1,13 +1,16 @@
+import * as vscode from 'vscode';
 import XmlFileFactory from "./XmlFileFactory";
 
 export default class XmlLayoutFileFactory extends XmlFileFactory
 {
-	public getXmlObject(): object {
+	async getXmlObject() {
+		const layoutType = await this.askForLayoutType();
+
 		return {
 			config: {
 				_attributes: {
 					"xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
-					"layout": "1column",
+					"layout": layoutType,
 					"xsi:noNamespaceSchemaLocation": "urn:magento:framework:View/Layout/etc/page_configuration.xsd",
 				},
 				body: {
@@ -21,4 +24,41 @@ export default class XmlLayoutFileFactory extends XmlFileFactory
 			}
 		};
 	}
+
+	/**
+	 * Ask for layout type
+	 */
+	protected async askForLayoutType(): Promise<string> {
+		return vscode.window.showQuickPick([
+			{
+				label: '1column',
+				description: 'One column layout'
+			},
+			{
+				label: '2columns-left',
+				description: 'Two columns layout with left bar'
+			},
+			{
+				label: '2columns-right',
+				description: 'Two columns layout with right bar'
+			},
+			{
+				label: '3columns',
+				description: 'Three columns layout'
+			},
+			{
+				label: 'empty',
+				description: 'Empty layout'
+			}
+		], {
+			placeHolder: 'Select layout type'
+		}).then((selected) => {
+			if (selected) {
+				return selected.label;
+			}
+
+			return '1column';
+		});
+	}
+
 }
